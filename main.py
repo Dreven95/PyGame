dafebd542fc55e0753442e081d350ba18664d67d
Imam95chr <131966547+Imam95chr@users.noreply.github.com>
2023-05-25 11:54:39 +0300

    Имам делает коммит
    Добавил весь код до основного цикла, вроде всё пашет. Прости меня Адам мой великий Господин

--- main.py
+++ main.py
@@ -21,3 +21,37 @@ walk_left = [
     pygame.image.load("image/pers_left/pers_left3.png").convert_alpha(),
     pygame.image.load("image/pers_left/pers_left4.png").convert_alpha(),
 ]
+
+cloud = pygame.image.load("image/cloud.png").convert_alpha()
+cloud_x = 842
+cloud_y = 50
+
+
+ghost = pygame.image.load("image/ghost.png").convert_alpha()
+ghost_list_in_game = []
+
+proigrysh = pygame.image.load("image/proigysh.png")
+
+player_anim = 0
+bg_x = 0
+player_speed = 10
+player_x = 100
+player_y = 415
+is_jump = False
+jump_count = 12
+bullet = pygame.image.load("image/bullet.png").convert_alpha()
+bullets = []
+bullets_num = 8
+
+
+# таймер для спавна призрака
+ghost_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(ghost_timer, 2000)
+
+
+mp3 = pygame.mixer.Sound("mp3/pawapepe_gemabody_Павапепе_гемабоди_х_За.mp3")
+mp3.play()
+
+game_play = True
+
+running = True
